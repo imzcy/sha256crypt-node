7e55c399f06a1c72fcf9c67b52da5db3920bfe44
Chenyang Zhu <chenyang.zhu@fomopay.com>
2017-08-29 13:03:58 +0800

    Add checks for salt format.

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ function verify(password, rounds, salt, derived) {
     if (rounds_n > 999999999) {
         throw new Error('Rounds must be at most 999999999.');
     }
+    if (!(/^[\.\/0-9A-Za-z]{0,16}$/.test(salt))) {
+        throw new Error('Salt must contain characters only from ./0-9A-Za-z and has a length between 0 and 16 characters (inclusive)');
+    }
     const padded_salt = '$5$rounds=' + rounds_n.toFixed(0) + '$' + salt;
     const hash = sha256crypt(password, padded_salt);
     const verify = `${padded_salt}$${derived}`;
@@ -21,6 +24,9 @@ function hash(password, rounds, salt) {
     }
     if (rounds_n > 999999999) {
         throw new Error('Rounds must be at most 999999999.');
+    }
+    if (!(/^[\.\/0-9A-Za-z]{0,16}$/.test(salt))) {
+        throw new Error('Salt must contain characters only from ./0-9A-Za-z');
     }
     const padded_salt = '$5$rounds=' + rounds_n.toFixed(0) + '$' + salt;
     const hash = sha256crypt(password, padded_salt);
